a55b35abfac659052a5f6b3e6d598ea21f28769c
Spencer Richey <richeyslr@gmail.com>
2021-09-22 18:27:29 -0500

    lab 2 event handler functioning

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 class HornedBeasts extends Component { 
+  constructor(props){
+    super(props);
+    this.state = {favs: 0};
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick = () => {
+    this.setState((state) => {return {favs: state.favs + 1}});
+  }
   render() {
     return (
       <div>
         <figure>
         <h2>{this.props.title}</h2>
-        <img src={this.props.img_url} alt={this.props.description} title={this.title} />
-        <p>{this.props.description} | </p>
+        <img width='300px' onClick={this.handleClick} src={this.props.img_url} alt={this.props.description} title={this.title} />
+        <p>{this.props.description} | &hearts; {this.state.favs}</p>
         </figure>
       </div>
     )
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react'
 import HornedBeasts from './HornedBeasts'
 import data from './data.json'
-import { bind } from 'lodash';
 
 export default class Main extends Component {
-  constructor(props){
-    super(props);
-    this.state = {favs: 0};
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick = () => {
-    this.setState((state) => {return {favs: state.favs + 1}});
-  }
   render() {
     return (
         <div>
